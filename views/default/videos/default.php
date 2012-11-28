1b74a999f11bd1e9e3bedef7200960601d349cb6
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2012-11-28 10:46:15 +0200

    Creates a video src tag only for the formats the video has been converted to.

--- views/default/videos/default.php
+++ views/default/videos/default.php
@@ -14,20 +14,13 @@ $download_url = elgg_get_site_url() . "video/download/{$video->getGUID()}";
 echo '<video width="320" height="240" controls="controls">';
 
 if ($vars['full_view']) {
-	$formats = array(
-		'webm' => 'webm',
-		'mp4' => 'mp4',
-		'mpg' => 'mpg',
-		'ogv' => 'ogg',
-		'ogg' => 'ogg',
-		'MOV' => 'MOV',
-	);
-	foreach ($formats as $format => $mime_type) {
+	$formats = $video->getConvertedFormats();
+	foreach ($formats as $format) {
 		$site_url = elgg_get_site_url();
 		$video_url = "{$site_url}mod/videos/video.php?video_guid={$video->guid}&format=$format";
 	
 	echo <<<HTML
-		<source src="$video_url" type="video/$mime_type">
+		<source src="$video_url" type="video/$format">
 HTML;
 	}
 }
