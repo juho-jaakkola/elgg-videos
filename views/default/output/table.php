8a15cf80914c96943a62a42b096e5e3c96509b61
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2012-12-07 15:05:01 +0200

    Added a view that displays an html table.

---
+++ views/default/output/table.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Table output
+ *
+ * @package Videos
+ *
+ * @uses $vars['table_class] The classes for table element
+ * @uses $vars['headers'] The column headers
+ * @uses $vars['rows'] Two dimensional array with rows and cells
+ *
+ */
+
+$table_class = array('class' => 'elgg-table');
+
+if (isset($vars['table_class'])) {
+	$table_class['class'] = $vars['table_class'];
+}
+$table_class = elgg_format_attributes($table_class);
+
+$header = '';
+if (isset($vars['headers'])) {
+	foreach ($vars['headers'] as $header) {
+		$headers .= "<th>$header</th>";
+	}
+	$headers = "<tr>$headers</tr>";
+}
+
+$rows = '';
+if (isset($vars['rows'])) {
+	foreach ($vars['rows'] as $row) {
+		$cells = '';
+		foreach ($row as $value) {
+			$cells .= "<td>$value</td>";
+		}
+		$rows .= "<tr>$cells</tr>";
+	}
+}
+
+echo "<table $table_class>{$headers}{$rows}</table>";
